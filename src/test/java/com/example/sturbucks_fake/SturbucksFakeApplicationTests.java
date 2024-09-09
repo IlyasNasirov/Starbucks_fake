dc992180dbf27518b874de04c5a9619275e043bc
i.nasirov <ilyasnasirov9@gmail.com>
2024-09-09 17:42:20 +0500

    Start

---
+++ src/test/java/com/example/sturbucks_fake/SturbucksFakeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sturbucks_fake;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SturbucksFakeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
